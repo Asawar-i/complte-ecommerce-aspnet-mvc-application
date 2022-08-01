215ddf072fd615fc145fac18418a8ce48e6b3b51
AsawariPatil <AsawariPatil@MESHBAKOP206>
2022-08-01 13:16:41 +0530

    Add project files.

---
+++ EeTicketsA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EeTicketsA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
